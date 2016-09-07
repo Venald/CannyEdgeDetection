18b63f229de9a799045a6f483dc0175c9ca94191
Vesa <vesa2006@gmail.com>
2016-09-07 12:37:33 +0300

    Questions added

--- KernelEdgeDetect/EdgeDetect.cs
+++ KernelEdgeDetect/EdgeDetect.cs
@@ -15,11 +15,11 @@ namespace CannyEdgeDetect
             Image loaded_image = Image.FromFile("Test.png");
             Bitmap imageMap = new Bitmap(loaded_image);
             Bitmap newImage = new Bitmap(loaded_image);
-            int K = 3;
+            int K = 3; //kernel size
             int[,] edgeDetectKernel = new int[3, 3] { { -1, -1, -1 },
                                                      {  -1, -8, -1 },
                                                      {  -1, -1, -1 }};
-            //  5 / 2 = Kernel size halfed
+            //  Kernel size halved for x and y
             for (int x = K / 2; x < imageMap.Width - K / 2; x++)
             {
                 for (int y = K / 2; y < imageMap.Height - K / 2; y++)
@@ -27,6 +27,8 @@ namespace CannyEdgeDetect
                     int sumR = 0;
                     int sumB = 0;
                     int sumG = 0;
+
+                    // Should go thru the kernel and x
                     for (int row = -K / 2; row < K / 2; row++)
                     {
                         for (int element = -K / 2; element < K / 2; element++)
@@ -40,16 +42,17 @@ namespace CannyEdgeDetect
 
                         }
                     }
+                    //After convolution the sums aren't always in [0,255]
                     if (sumR < 0) sumR *= -1;
-                    if (sumB < 0) sumB *= -1;
                     if (sumG < 0) sumG *= -1;
+                    if (sumB < 0) sumB *= -1;
                     sumR /= 16;
                     sumG /= 16;
                     sumB /= 16;
                     if (sumR > 255) sumR = 255;
                     if (sumB > 255) sumB = 255;
                     if (sumG > 255) sumG = 255;
-
+                    //No processing in alpha channel should there be?
                     int sumA = imageMap.GetPixel(x, y).A;
                     newImage.SetPixel(x, y, Color.FromArgb(sumA, sumR, sumG, sumB));
                 }
